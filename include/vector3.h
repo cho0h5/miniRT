2af41b18959b3143ac5b12900e0267885a6804f0
Youngho Cho <dominoyh5@gmail.com>
2024-02-19 17:13:57 +0900

    fix: return type in vector3.h

--- include/vector3.h
+++ include/vector3.h
@@ -20,8 +20,8 @@ typedef struct s_vector3
 	double	z;
 }	t_vector3;
 
-s_vector3	vector3(double x, double y, double z);
+t_vector3	vector3(double x, double y, double z);
 double		magnitude(const t_vector3 vec);
-s_vector3	normalize(const t_vector3 vec);
+t_vector3	normalize(const t_vector3 vec);
 
 #endif
