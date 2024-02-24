aeb1f683869af328bccecfa10737d3e60c0674da
Youngho Cho <dominoyh5@gmail.com>
2024-02-24 18:19:33 +0900

    fix: ft_strncmp error (add ' == 0')

--- src/parse_identifier.c
+++ src/parse_identifier.c
@@ -10,17 +10,17 @@ t_identifier	parse_identifier(char *line, size_t *i)
 	skip_space(line, i);
 	id = ID_AMBIENT;
 	word = parse_word(line, i);
-	if (ft_strncmp(word, "A", 2))
+	if (ft_strncmp(word, "A", 2) == 0)
 		id = ID_AMBIENT;
-	else if (ft_strncmp(word, "C", 2))
+	else if (ft_strncmp(word, "C", 2) == 0)
 		id = ID_CAMERA;
-	else if (ft_strncmp(word, "L", 2))
+	else if (ft_strncmp(word, "L", 2) == 0)
 		id = ID_LIGHT;
-	else if (ft_strncmp(word, "pl", 3))
+	else if (ft_strncmp(word, "pl", 3) == 0)
 		id = ID_SPHERE;
-	else if (ft_strncmp(word, "sp", 3))
+	else if (ft_strncmp(word, "sp", 3) == 0)
 		id = ID_PLANE;
-	else if (ft_strncmp(word, "cy", 3))
+	else if (ft_strncmp(word, "cy", 3) == 0)
 		id = ID_CYLINDER;
 	else
 		panic("unhandled identifier");
