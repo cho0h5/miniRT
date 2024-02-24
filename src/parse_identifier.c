5c49bc86d8f68979dd825a42ea2d4d459d832e39
Youngho Cho <dominoyh5@gmail.com>
2024-02-24 16:56:04 +0900

    add: parse_identifier()

--- src/parse_identifier.c
+++ src/parse_identifier.c
@@ -1,9 +1,29 @@
+#include "libft.h"
+#include "panic.h"
 #include "parse.h"
 
 t_identifier	parse_identifier(char *line, size_t *i)
 {
-	char	*identifier;
+	char			*word;
+	t_identifier	id;
+
 	skip_space(line, i);
-	identifier = parse_word(line, i);
-	return (ID_AMBIENT);
+	id = ID_AMBIENT;
+	word = parse_word(line, i);
+	if (ft_strncmp(word, "A", 2))
+		id = ID_AMBIENT;
+	else if (ft_strncmp(word, "C", 2))
+		id = ID_CAMERA;
+	else if (ft_strncmp(word, "L", 2))
+		id = ID_LIGHT;
+	else if (ft_strncmp(word, "pl", 3))
+		id = ID_SPHERE;
+	else if (ft_strncmp(word, "sp", 3))
+		id = ID_PLANE;
+	else if (ft_strncmp(word, "cy", 3))
+		id = ID_CYLINDER;
+	else
+		panic("unhandled identifier");
+	free(word);
+	return (id);
 }
