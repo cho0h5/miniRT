756f37a53b7ff37717aaad4662fa5717f4631fd1
Youngho Cho <dominoyh5@gmail.com>
2024-02-26 17:25:35 +0900

    update: parse_light()

--- src/parse_light.c
+++ src/parse_light.c
@@ -1,8 +1,22 @@
+#include "libft.h"
+#include "object.h"
+#include "panic.h"
 #include "parse.h"
 
 void	parse_light(t_list **lights, const char *line, size_t *i)
 {
-	(void)lights;
-	(void)line;
-	(void)i;
+	t_light	*light;
+	t_list	*node;
+
+	light = malloc(sizeof(t_light));
+	if (light == NULL)
+		panic("failed to malloc");
+	parse_vector3(&light->position, line, i);
+	parse_decimal(&light->brightness, line, i);
+	parse_vector3(&light->color, line, i);
+	parse_newline(line, i);
+	node = ft_lstnew(light);
+	if (node == NULL)
+		panic("failed to malloc");
+	ft_lstadd_back(lights, node);
 }
