114067d9f276d52f718daa809f5e44f2e6df4df8
Youngho Cho <dominoyh5@gmail.com>
2024-03-01 15:20:45 +0900

    update: change get_normilized_ray()'s signature

--- src/screen_pixel_to_ray.c
+++ src/screen_pixel_to_ray.c
@@ -2,9 +2,9 @@
 #include "screen.h"
 #include "vector3.h"
 
-static t_vector3	get_normalized_ray(const t_camera *camera, int i, int j)
+static t_vector3	get_normalized_ray(double fov, int i, int j)
 {
-	const double	dx = tan((camera->fov / 2) * (M_PI / 180)) / ((double)WIDTH / 2);
+	const double	dx = tan((fov / 2) * (M_PI / 180)) / ((double)WIDTH / 2);
 	t_vector3		ray;
 
 	ray.x = (i - (double)WIDTH / 2) * dx;
@@ -15,5 +15,6 @@ static t_vector3	get_normalized_ray(const t_camera *camera, int i, int j)
 
 t_vector3	pixel_to_ray(const t_camera *camera, int i, int j)
 {
-	return (get_normalized_ray(camera, i, j));
+	const t_vector3 normalized_ray = get_normalized_ray(camera->fov, i, j);
+	return (normalized_ray);
 }
