d970473324920763844c3fb443fc9f10957e6dc1
Youngho Cho <dominoyh5@gmail.com>
2024-03-01 17:03:41 +0900

    fix: change normalization timing

--- src/vector3_rotate.c
+++ src/vector3_rotate.c
@@ -7,7 +7,7 @@ t_rotate_info	init_rotate_info(const t_camera *camera)
 	const double	theta = acos(angle(camera->orientation, vector3(0, 1, 0)));
 	t_rotate_info	info;
 
-	info.axis_term = normalize(cross(vector3(0, 1, 0), camera->orientation));
+	info.axis_term = cross(vector3(0, 1, 0), camera->orientation);
 	info.sin_term = sin(theta);
 	info.cos_term = cos(theta);
 	return (info);
@@ -18,9 +18,11 @@ t_vector3	rotate(const t_rotate_info *info, const t_vector3 vec)
 	t_vector3	tmp1;
 	t_vector3	tmp2;
 	t_vector3	tmp3;
+	t_vector3	tmp4;
 
 	tmp1 = scale(info->axis_term, dot(info->axis_term, vec) * (1 - info->cos_term));
 	tmp2 = scale(vec, info->cos_term);
 	tmp3 = scale(cross(info->axis_term, vec), info->sin_term);
-	return (add(tmp1, add(tmp2, tmp3)));
+	tmp4 = add(tmp1, add(tmp2, tmp3));
+	return (normalize(tmp4));
 }
