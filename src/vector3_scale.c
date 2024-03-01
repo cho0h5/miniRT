81c0fb2a4d7b464f1417475978497fdc2985ea06
Youngho Cho <dominoyh5@gmail.com>
2024-03-01 15:59:49 +0900

    fix: change '-' to '*' in scale()

--- src/vector3_scale.c
+++ src/vector3_scale.c
@@ -4,8 +4,8 @@ t_vector3	scale(const t_vector3 vec, double a)
 {
 	t_vector3	result;
 
-	result.x = vec.x - a;
-	result.y = vec.y - a;
-	result.z = vec.z - a;
+	result.x = vec.x * a;
+	result.y = vec.y * a;
+	result.z = vec.z * a;
 	return (result);
 }
