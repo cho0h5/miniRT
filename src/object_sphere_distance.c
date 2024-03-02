c23238f3fee9c468ab490bd78a6ce74ad06807c9
Youngho Cho <dominoyh5@gmail.com>
2024-03-02 13:45:44 +0900

    update: return distance in sphere_distance()

--- src/object_sphere_distance.c
+++ src/object_sphere_distance.c
@@ -1,20 +1,20 @@
+#include <math.h>
 #include "object.h"
 #include "vector3.h"
 
 double	sphere_distance(const t_vector3 pos, const t_vector3 ray, const t_sphere *sphere)
 {
+	const t_vector3	ps_vec = subtract(pos, sphere->position);
 	const double	radius = sphere->diameter / 2;
-	t_vector3	ps_vec;
-	double		b;
-	double		c;
-	double		discriminant;
+	const double	b = 2 * dot(ps_vec, ray);
+	const double	c = dot(ps_vec, ps_vec) - radius * radius;
+	const double	discriminant = b * b - 4 * c;
 
-	ps_vec = subtract(pos, sphere->position);
-	b = 2 * dot(ps_vec, ray);
-	c = dot(ps_vec, ps_vec) - radius * radius;
-	discriminant = b * b - 4 * c;
-	if (discriminant >= 0)
-		return (1);
-
+	if (discriminant < 0)
+		return (DOUBLE_MAX);
+	if (-b - sqrt(discriminant) >= 0)
+		return ((-b - sqrt(discriminant)) / 2);
+	if (-b + sqrt(discriminant) >= 0)
+		return ((-b + sqrt(discriminant)) / 2);
 	return (DOUBLE_MAX);
 }
