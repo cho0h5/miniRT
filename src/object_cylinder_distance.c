ca1fecd0b4e34fd7f621988758bec317486c04a9
Youngho Cho <dominoyh5@gmail.com>
2024-03-02 17:47:28 +0900

    update: maybe.. it's cylinder

--- src/object_cylinder_distance.c
+++ src/object_cylinder_distance.c
@@ -1,10 +1,13 @@
+#include <math.h>
 #include "object.h"
 #include "vector3.h"
 
 double	cylinder_distance(const t_vector3 pos, const t_vector3 ray, const t_cylinder *cylinder)
 {
-	(void)pos;
-	(void)ray;
-	(void)cylinder;
+	const t_vector3	normal_vec = normalize(cross(ray, cylinder->axis));
+	const double	distance = dot(subtract(pos, cylinder->position), normal_vec);
+
+	if (fabs(distance) <= cylinder->diameter / 2)
+		return (distance);
 	return (DOUBLE_MAX);
 }
