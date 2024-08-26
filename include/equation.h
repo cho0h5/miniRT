f6139c256afd6ea2224aa71ed65b55fd0875664d
Youngho Cho <dominoyh5@gmail.com>
2024-08-26 19:36:57 +0900

    remove: closest_point_on_skew_lines_s (because of duality)

--- include/equation.h
+++ include/equation.h
@@ -6,7 +6,7 @@
 /*   By: younghoc <younghoc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/26 19:12:27 by younghoc          #+#    #+#             */
-/*   Updated: 2024/08/26 19:22:20 by younghoc         ###   ########.fr       */
+/*   Updated: 2024/08/26 19:35:41 by younghoc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,7 @@
 
 # include "vector3.h"
 
-double closest_point_on_skew_lines_t(const t_vector3 a1, const t_vector3 b1,
-    const t_vector3 a2, const t_vector3 b2);
-double closest_point_on_skew_lines_s(const t_vector3 a1, const t_vector3 b1,
+double closest_point_on_skew_lines(const t_vector3 a1, const t_vector3 b1,
     const t_vector3 a2, const t_vector3 b2);
 
 #endif
--- src/equation_closest_point_on_skew_lines.c
+++ src/equation_closest_point_on_skew_lines.c
@@ -6,13 +6,13 @@
 /*   By: younghoc <younghoc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/26 19:20:29 by younghoc          #+#    #+#             */
-/*   Updated: 2024/08/26 19:22:18 by younghoc         ###   ########.fr       */
+/*   Updated: 2024/08/26 19:35:30 by younghoc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "equation.h"
 
-double closest_point_on_skew_lines_t(const t_vector3 a1, const t_vector3 b1,
+double closest_point_on_skew_lines(const t_vector3 a1, const t_vector3 b1,
     const t_vector3 a2, const t_vector3 b2)
 {
     const t_vector3 a2a1 = subtract(a1, a2);
@@ -21,17 +21,5 @@ double closest_point_on_skew_lines_t(const t_vector3 a1, const t_vector3 b1,
     const double    b1b2 = dot(b1, b2);
 
     return (dot(a2a1, b2) * b1b2 - dot(a2a1, b1) * b2square)
-        / (b1square * b2square - b1b2 * b1b2);
-}
-
-double closest_point_on_skew_lines_s(const t_vector3 a1, const t_vector3 b1,
-    const t_vector3 a2, const t_vector3 b2)
-{
-    const t_vector3 a2a1 = subtract(a1, a2);
-    const double    b1square = dot(b1, b1);
-    const double    b2square = dot(b2, b2);
-    const double    b1b2 = dot(b1, b2);
-
-    return (dot(a2a1, b1) * b1b2 - dot(a2a1, b2) * b1square)
         / (b1square * b2square - b1b2 * b1b2);
 }
