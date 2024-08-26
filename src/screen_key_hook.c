119e6ff94a39f770a9a25ee5b199118f5751fe4c
Youngho Cho <dominoyh5@gmail.com>
2024-08-26 15:07:13 +0900

    norm: sungyoon

--- src/screen_key_hook.c
+++ src/screen_key_hook.c
@@ -6,7 +6,7 @@
 /*   By: younghoc <younghoc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/26 13:52:45 by younghoc          #+#    #+#             */
-/*   Updated: 2024/08/26 13:52:46 by younghoc         ###   ########.fr       */
+/*   Updated: 2024/08/26 15:03:59 by younghoc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,19 +22,23 @@ static void	move_camera(int keycode, t_environment *env)
 {
 	const t_vector3	pos = get_camera(env)->position;
 	const t_vector3	dir = get_camera(env)->orientation;
+	t_camera		*camera;
 
+	camera = get_camera(env);
 	if (keycode == KEY_Q)
-		get_camera(env)->position.z -= 10;
+		camera->position.z -= 10;
 	else if (keycode == KEY_E)
-		get_camera(env)->position.z += 10;
+		camera->position.z += 10;
 	else if (keycode == KEY_W)
-		get_camera(env)->position = add(pos, scale(dir, 10));
+		camera->position = add(pos, scale(dir, 10));
 	else if (keycode == KEY_S)
-		get_camera(env)->position = add(pos, scale(dir, -10));
+		camera->position = add(pos, scale(dir, -10));
 	else if (keycode == KEY_A)
-		get_camera(env)->position = add(pos, scale(normalize(cross(vector3(0, 0, 1), dir)), 10));
+		camera->position = add(pos,
+				scale(normalize(cross(vector3(0, 0, 1), dir)), 10));
 	else if (keycode == KEY_D)
-		get_camera(env)->position = add(pos, scale(normalize(cross(vector3(0, 0, 1), dir)), -10));
+		camera->position = add(pos,
+				scale(normalize(cross(vector3(0, 0, 1), dir)), -10));
 	else
 		panic("failed to move camera: unexpected key");
 }
@@ -42,19 +46,21 @@ static void	move_camera(int keycode, t_environment *env)
 static void	rotate_camera(int keycode, t_environment *env)
 {
 	const t_vector3	dir = get_camera(env)->orientation;
+	t_camera		*camera;
 
+	camera = get_camera(env);
 	if (keycode == KEY_UP)
-		get_camera(env)->orientation = rotate2(normalize(
-				cross(dir, vector3(0, 0, 1))), 10.0 / 180 * M_PI, dir);
+		camera->orientation = rotate2(normalize(
+					cross(dir, vector3(0, 0, 1))), 10.0 / 180 * M_PI, dir);
 	else if (keycode == KEY_DOWN)
-		get_camera(env)->orientation = rotate2(normalize(
-				cross(dir, vector3(0, 0, 1))), -10.0 / 180 * M_PI, dir);
+		camera->orientation = rotate2(normalize(
+					cross(dir, vector3(0, 0, 1))), -10.0 / 180 * M_PI, dir);
 	else if (keycode == KEY_LEFT)
-		get_camera(env)->orientation = rotate2(vector3(0, 0, 1),
-						10.0 / 180 * M_PI, dir);
+		camera->orientation = rotate2(vector3(0, 0, 1),
+				10.0 / 180 * M_PI, dir);
 	else if (keycode == KEY_RIGHT)
-		get_camera(env)->orientation = rotate2(vector3(0, 0, 1),
-						-10.0 / 180 * M_PI, dir);
+		camera->orientation = rotate2(vector3(0, 0, 1),
+				-10.0 / 180 * M_PI, dir);
 }
 
 int	key_hook(int keycode, t_environment *env)
