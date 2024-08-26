d799242131325ae4d24c9a152e7e030b70985d41
Youngho Cho <dominoyh5@gmail.com>
2024-08-26 14:29:52 +0900

    Revert "norm: include/"
    This reverts commit d4adc17aa72f46bdc4673f873d0be78e84ce420a.

--- include/object.h
+++ include/object.h
@@ -3,17 +3,18 @@
 /*                                                        :::      ::::::::   */
 /*   object.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: younghoc <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: younghoc <younghoc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/19 15:45:32 by younghoc          #+#    #+#             */
-/*   Updated: 2024/02/19 15:45:34 by younghoc         ###   ########.fr       */
+/*   Updated: 2024/08/26 13:53:40 by younghoc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef OBJECT_H
 # define OBJECT_H
 
-# include "vector3.h"
+#include "libft.h"
+#include "vector3.h"
 
 typedef struct s_ambient
 {
@@ -65,14 +66,25 @@ typedef struct s_cylinder
 	t_vector3	color;
 }	t_cylinder;
 
-void			ambient_print(const t_ambient *ambient);
-void			camera_print(const t_camera *camera);
-void			light_print(const t_light *light);
-void			object_print(const t_object_category *object_category);
-void			plane_print(const t_plane *plane);
-void			sphere_print(const t_sphere *sphere);
-void			cylinder_print(const t_cylinder *cylinder);
+void				ambient_print(const t_ambient *ambient);
+void				camera_print(const t_camera *camera);
+void				light_print(const t_light *light);
+void				object_print(const t_object_category *object_category);
+void				plane_print(const t_plane *plane);
+void				sphere_print(const t_sphere *sphere);
+void				cylinder_print(const t_cylinder *cylinder);
 
-t_rotate_info	init_rotate_info(const t_camera *camera);
+t_rotate_info		init_rotate_info(const t_camera *camera);
+
+t_object_category	*get_closest_object(const t_vector3 pos, const t_vector3 ray, t_list *objects);
+double				object_distance(const t_vector3 pos, const t_vector3 ray, const t_object_category *object_category);
+double				plane_distance(const t_vector3 pos, const t_vector3 ray, const t_plane *plane);
+double				sphere_distance(const t_vector3 pos, const t_vector3 ray, const t_sphere *sphere);
+double				cylinder_distance(const t_vector3 pos, const t_vector3 ray, const t_cylinder *cylinder);
+
+unsigned int		get_object_color(const t_object_category *object_category);
+unsigned int		get_plane_color(const t_plane *plane);
+unsigned int		get_sphere_color(const t_sphere *sphere);
+unsigned int		get_cylinder_color(const t_cylinder *cylinder);
 
 #endif
