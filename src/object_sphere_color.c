65789675ad7ece94f3f5bf86b9a868ac58cf9af9
Youngho Cho <dominoyh5@gmail.com>
2024-08-29 16:08:37 +0900

    add: light brightness

--- src/object_sphere_color.c
+++ src/object_sphere_color.c
@@ -6,7 +6,7 @@
 /*   By: younghoc <younghoc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/26 13:52:17 by younghoc          #+#    #+#             */
-/*   Updated: 2024/08/29 15:56:17 by younghoc         ###   ########.fr       */
+/*   Updated: 2024/08/29 16:08:03 by younghoc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,8 +32,8 @@ static t_vector3	get_sphere_diffuse(const t_sphere *sphere,
 	const t_vector3	normal = normalize(subtract(hit_point, sphere->position));
 	const t_vector3	to_light = normalize(
 			subtract(get_light(env)->position, hit_point));
-	const double	diffuse_strength = fmax(0, dot(normal, to_light));
-	const t_vector3	diffuse = scale(get_light(env)->color, diffuse_strength);
+	const double	diffuse_strength = fmax(0, dot(normal, to_light)) * get_light(env)->brightness;
+	const t_vector3	diffuse = multiply(sphere->color, scale(get_light(env)->color, diffuse_strength / 255));
 
 	return (diffuse);
 }
