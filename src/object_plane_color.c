921d209a679121d4d3c129c7835a546f66805f79
Youngho Cho <dominoyh5@gmail.com>
2024-08-29 13:41:48 +0900

    norm

--- src/object_plane_color.c
+++ src/object_plane_color.c
@@ -6,7 +6,7 @@
 /*   By: younghoc <younghoc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/26 13:52:09 by younghoc          #+#    #+#             */
-/*   Updated: 2024/08/29 13:10:52 by younghoc         ###   ########.fr       */
+/*   Updated: 2024/08/29 13:41:39 by younghoc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,11 @@
 #include "color.h"
 #include "vector3.h"
 
-static t_vector3	get_plane_ambient(const t_plane *plane, const t_ambient *ambient)
+static t_vector3	get_plane_ambient(const t_plane *plane,
+		const t_ambient *ambient)
 {
-	return (multiply(plane->color, scale(ambient->color, ambient->ratio / 255)));
+	return (multiply(plane->color,
+			scale(ambient->color, ambient->ratio / 255)));
 }
 
 unsigned int	get_plane_color(const t_plane *plane,
@@ -25,7 +27,7 @@ unsigned int	get_plane_color(const t_plane *plane,
 {
 	const t_vector3	ambient = get_plane_ambient(plane, get_ambient(env));
 	const t_vector3	hit_point = add(get_camera(env)->position,
-		scale(get_camera(env)->orientation, distance));
+			scale(get_camera(env)->orientation, distance));
 
 	(void)hit_point;
 	return (to_uint(ambient));
--- src/object_sphere_color.c
+++ src/object_sphere_color.c
@@ -6,7 +6,7 @@
 /*   By: younghoc <younghoc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/26 13:52:17 by younghoc          #+#    #+#             */
-/*   Updated: 2024/08/29 13:38:17 by younghoc         ###   ########.fr       */
+/*   Updated: 2024/08/29 13:40:52 by younghoc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,11 @@
 #include "color.h"
 #include "vector3.h"
 
-static t_vector3	get_sphere_ambient(const t_sphere *sphere, const t_ambient *ambient)
+static t_vector3	get_sphere_ambient(const t_sphere *sphere,
+		const t_ambient *ambient)
 {
-	return (multiply(sphere->color, scale(ambient->color, ambient->ratio / 255)));
+	return (multiply(sphere->color,
+			scale(ambient->color, ambient->ratio / 255)));
 }
 
 static t_vector3	get_sphere_diffuse(const t_sphere *sphere,
@@ -27,8 +29,9 @@ static t_vector3	get_sphere_diffuse(const t_sphere *sphere,
 	const t_vector3	hit_point = add(get_camera(env)->position,
 			scale(get_camera(env)->orientation, distance));
 	const t_vector3	normal = normalize(subtract(hit_point, sphere->position));
-	const t_vector3	to_light = normalize(subtract(get_light(env)->position, hit_point));
-	const double diffuse_strength = fmax(0, dot(normal, to_light));
+	const t_vector3	to_light = normalize(
+			subtract(get_light(env)->position, hit_point));
+	const double	diffuse_strength = fmax(0, dot(normal, to_light));
 	const t_vector3	diffuse = scale(get_light(env)->color, diffuse_strength);
 
 	return (diffuse);
