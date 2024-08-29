6457cf3238a79db528306f88c0e58feb76812dff
Youngho Cho <dominoyh5@gmail.com>
2024-08-29 12:46:00 +0900

    norm

--- src/object_cylinder_color.c
+++ src/object_cylinder_color.c
@@ -6,14 +6,13 @@
 /*   By: younghoc <younghoc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/26 13:51:54 by younghoc          #+#    #+#             */
-/*   Updated: 2024/08/29 12:34:27 by younghoc         ###   ########.fr       */
+/*   Updated: 2024/08/29 12:45:56 by younghoc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "environment.h"
 #include "object.h"
 #include "color.h"
-
 
 unsigned int	get_cylinder_color(const t_cylinder *cylinder,
 						const t_environment *env)
