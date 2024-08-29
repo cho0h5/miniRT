d4b9c61ea680f8f61d85e9f88cbcd11e7ba7979e
Youngho Cho <dominoyh5@gmail.com>
2024-08-29 13:14:31 +0900

    add: sphere_ambient

--- src/object_sphere_color.c
+++ src/object_sphere_color.c
@@ -6,18 +6,25 @@
 /*   By: younghoc <younghoc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/26 13:52:17 by younghoc          #+#    #+#             */
-/*   Updated: 2024/08/29 12:53:25 by younghoc         ###   ########.fr       */
+/*   Updated: 2024/08/29 13:13:37 by younghoc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "environment.h"
 #include "object.h"
 #include "color.h"
+#include "vector3.h"
+
+static t_vector3	get_sphere_ambient(const t_sphere *sphere, const t_ambient *ambient)
+{
+	return (multiply(sphere->color, scale(ambient->color, ambient->ratio / 255)));
+}
 
 unsigned int	get_sphere_color(const t_sphere *sphere,
 						const t_environment *env, const double distance)
 {
-	(void)env;
+	const t_vector3	ambient = get_sphere_ambient(sphere, get_ambient(env));
+
 	(void)distance;
-	return (to_uint(sphere->color));
+	return (to_uint(ambient));
 }
