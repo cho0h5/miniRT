c5d10ba0ee889d2e11c12f4c0796cede09cb3e02
Youngho Cho <dominoyh5@gmail.com>
2024-08-29 20:07:12 +0900

    add: cylinder diffuse skel

--- src/object_cylinder_color.c
+++ src/object_cylinder_color.c
@@ -6,7 +6,7 @@
 /*   By: younghoc <younghoc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/26 13:51:54 by younghoc          #+#    #+#             */
-/*   Updated: 2024/08/29 20:03:53 by younghoc         ###   ########.fr       */
+/*   Updated: 2024/08/29 20:06:41 by younghoc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,13 +21,27 @@ static t_vector3	get_cylinder_ambient(const t_cylinder *cylinder,
 			scale(ambient->color, ambient->ratio / 255)));
 }
 
+static t_vector3	get_cylinder_diffuse(const t_cylinder *cylinder,
+						const t_environment *env, const t_vector3 ray,
+						const double distance)
+{
+	(void)cylinder;
+	(void)env;
+	(void)ray;
+	(void)distance;
+	const t_vector3	asdf = vector3(0, 0, 0);
+	return (asdf);
+}
+
 unsigned int	get_cylinder_color(const t_cylinder *cylinder,
 						const t_environment *env, const t_vector3 ray,
 						const double distance)
 {
 	const t_vector3	ambient = get_cylinder_ambient(cylinder, get_ambient(env));
+	const t_vector3	diffuse = get_cylinder_diffuse(cylinder, env, ray, distance);
 
-	(void)ray;
-	(void)distance;
-	return (to_color(ambient));
+	if (is_shadow(env, ray, distance))
+		return (to_color(ambient));
+	else
+		return (to_color(add(ambient, diffuse)));
 }
