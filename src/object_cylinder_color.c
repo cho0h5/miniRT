2a9f23493d782086446e8f444f3d887a45fc28ad
Youngho Cho <dominoyh5@gmail.com>
2024-08-29 20:19:27 +0900

    norm

--- src/object_cylinder_color.c
+++ src/object_cylinder_color.c
@@ -6,13 +6,15 @@
 /*   By: younghoc <younghoc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/26 13:51:54 by younghoc          #+#    #+#             */
-/*   Updated: 2024/08/29 20:06:41 by younghoc         ###   ########.fr       */
+/*   Updated: 2024/08/29 20:19:20 by younghoc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <math.h>
 #include "environment.h"
 #include "object.h"
 #include "color.h"
+#include "equation.h"
 
 static t_vector3	get_cylinder_ambient(const t_cylinder *cylinder,
 		const t_ambient *ambient)
@@ -25,12 +27,20 @@ static t_vector3	get_cylinder_diffuse(const t_cylinder *cylinder,
 						const t_environment *env, const t_vector3 ray,
 						const double distance)
 {
-	(void)cylinder;
-	(void)env;
-	(void)ray;
-	(void)distance;
-	const t_vector3	asdf = vector3(0, 0, 0);
-	return (asdf);
+	const double	s = closest_point_on_skew_lines(get_camera(env)->position,
+			ray, cylinder->position, cylinder->axis);
+	const t_vector3	center = add(cylinder->position, scale(cylinder->axis, s));
+	const t_vector3	hit_point = add(get_camera(env)->position,
+			scale(ray, distance));
+	const t_vector3	normal = normalize(subtract(hit_point, center));
+	const t_vector3	to_light = normalize(
+			subtract(get_light(env)->position, hit_point));
+	const double	diffuse_strength = fmax(0, dot(normal, to_light))
+		* get_light(env)->brightness;
+	const t_vector3	diffuse = multiply(cylinder->color,
+			scale(get_light(env)->color, diffuse_strength / 255));
+
+	return (diffuse);
 }
 
 unsigned int	get_cylinder_color(const t_cylinder *cylinder,
@@ -38,7 +48,8 @@ unsigned int	get_cylinder_color(const t_cylinder *cylinder,
 						const double distance)
 {
 	const t_vector3	ambient = get_cylinder_ambient(cylinder, get_ambient(env));
-	const t_vector3	diffuse = get_cylinder_diffuse(cylinder, env, ray, distance);
+	const t_vector3	diffuse = get_cylinder_diffuse(cylinder, env, ray,
+			distance);
 
 	if (is_shadow(env, ray, distance))
 		return (to_color(ambient));
