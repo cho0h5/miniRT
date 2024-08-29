fa8c2afdaff41fc530e22ae3dd401eff49934138
Youngho Cho <dominoyh5@gmail.com>
2024-08-29 13:11:01 +0900

    fix: ambient bug

--- src/object_plane_color.c
+++ src/object_plane_color.c
@@ -6,7 +6,7 @@
 /*   By: younghoc <younghoc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/26 13:52:09 by younghoc          #+#    #+#             */
-/*   Updated: 2024/08/29 13:04:29 by younghoc         ###   ########.fr       */
+/*   Updated: 2024/08/29 13:10:52 by younghoc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 
 static t_vector3	get_plane_ambient(const t_plane *plane, const t_ambient *ambient)
 {
-	return (multiply(plane->color, scale(ambient->color, ambient->ratio)));
+	return (multiply(plane->color, scale(ambient->color, ambient->ratio / 255)));
 }
 
 unsigned int	get_plane_color(const t_plane *plane,
