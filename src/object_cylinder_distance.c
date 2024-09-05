fe96c880b71e800df584ed9c5edffec2c4711c99
Youngho Cho <dominoyh5@gmail.com>
2024-09-05 12:56:36 +0900

    norm

--- src/object_cylinder_distance.c
+++ src/object_cylinder_distance.c
@@ -6,7 +6,7 @@
 /*   By: younghoc <younghoc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/26 13:51:57 by younghoc          #+#    #+#             */
-/*   Updated: 2024/08/26 19:47:31 by younghoc         ###   ########.fr       */
+/*   Updated: 2024/09/05 12:56:15 by younghoc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,40 @@
 #include "vector3.h"
 #include "equation.h"
 
+static double	calculate_hypotenuse_length(const t_vector3 ray,
+				const t_cylinder *cylinder, const double distance_skew)
+{
+	const double	hypotenuse_on_plane = sqrt((cylinder->diameter / 2)
+			* (cylinder->diameter / 2) - distance_skew * distance_skew);
+	const double	theta = fabs(acos(angle(cylinder->axis, ray)));
+
+	return (hypotenuse_on_plane / sin(theta));
+}
+
+static double	calculate_height_length(const t_vector3 ray,
+				const t_cylinder *cylinder, const double distance_skew)
+{
+	const double	hypotenuse_on_plane = sqrt((cylinder->diameter / 2)
+			* (cylinder->diameter / 2) - distance_skew * distance_skew);
+	const double	theta = fabs(acos(angle(cylinder->axis, ray)));
+
+	return (hypotenuse_on_plane / tan(theta));
+}
+
 double	cylinder_distance(const t_vector3 pos, const t_vector3 ray,
 				const t_cylinder *cylinder)
 {
 	const t_vector3	normal_vec = normalize(cross(ray, cylinder->axis));
-	const double	distance = dot(subtract(pos, cylinder->position),
+	const double	distance_skew = dot(subtract(pos, cylinder->position),
 			normal_vec);
 	const double	t = closest_point_on_skew_lines(pos, ray,
 			cylinder->position, cylinder->axis);
 	const double	s = closest_point_on_skew_lines(cylinder->position,
 			cylinder->axis, pos, ray);
 
-	if (fabs(distance) <= cylinder->diameter / 2
-		&& fabs(s) <= cylinder->height / 2)
-		return (t);
+	if (fabs(distance_skew) <= cylinder->diameter / 2
+		&& fabs(s - calculate_height_length(ray, cylinder, distance_skew))
+		<= cylinder->height / 2)
+		return (t - calculate_hypotenuse_length(ray, cylinder, distance_skew));
 	return (DOUBLE_MAX);
 }
