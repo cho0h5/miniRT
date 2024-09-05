1c1c09fd2ef0f4f786479bda43cc184b3c22beb3
Youngho Cho <dominoyh5@gmail.com>
2024-09-05 18:36:37 +0900

    update: sphere normal

--- src/object_sphere_color.c
+++ src/object_sphere_color.c
@@ -6,7 +6,7 @@
 /*   By: younghoc <younghoc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/26 13:52:17 by younghoc          #+#    #+#             */
-/*   Updated: 2024/08/29 19:16:45 by younghoc         ###   ########.fr       */
+/*   Updated: 2024/09/05 18:32:59 by younghoc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,13 +23,26 @@ static t_vector3	get_sphere_ambient(const t_sphere *sphere,
 			scale(ambient->color, ambient->ratio / 255)));
 }
 
+static t_vector3	get_normal_vector(const t_vector3 hit_point,
+		const t_vector3 sphere_position, const t_vector3 ray)
+{
+	const t_vector3	sphere_normal = normalize(subtract(hit_point,
+				sphere_position));
+
+	if (dot(sphere_normal, ray) < 0)
+		return (sphere_normal);
+	else
+		return (scale(sphere_normal, -1));
+}
+
 static t_vector3	get_sphere_diffuse(const t_sphere *sphere,
 						const t_environment *env, const t_vector3 ray,
 						const double distance)
 {
 	const t_vector3	hit_point = add(get_camera(env)->position,
 			scale(ray, distance));
-	const t_vector3	normal = normalize(subtract(hit_point, sphere->position));
+	const t_vector3	normal = get_normal_vector(hit_point, sphere->position,
+			ray);
 	const t_vector3	to_light = normalize(
 			subtract(get_light(env)->position, hit_point));
 	const double	diffuse_strength = fmax(0, dot(normal, to_light))
