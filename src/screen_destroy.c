7c7a5cadde0753f64f8b92b882146dc9f324e130
Youngho Cho <dominoyh5@gmail.com>
2024-09-06 20:06:14 +0900

    add: free_environment to screen_destroy

--- src/screen_destroy.c
+++ src/screen_destroy.c
@@ -6,7 +6,7 @@
 /*   By: younghoc <younghoc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/26 13:52:42 by younghoc          #+#    #+#             */
-/*   Updated: 2024/08/26 13:52:43 by younghoc         ###   ########.fr       */
+/*   Updated: 2024/09/06 20:05:30 by younghoc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,7 @@
 
 int	destroy_screen(t_environment *env)
 {
-	mlx_destroy_image(env->mlx, env->image.image);
-	mlx_destroy_window(env->mlx, env->mlx_window);
+	free_environment(env);
 	exit(0);
 	return (0);
 }
