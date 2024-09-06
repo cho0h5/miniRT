a4fd22de20608fbfd28d1cb0898b7c4cff679090
Youngho Cho <dominoyh5@gmail.com>
2024-09-06 16:58:00 +0900

    update: environmetn free

--- src/environment_free.c
+++ src/environment_free.c
@@ -10,4 +10,5 @@ void	free_environment(t_environment *env)
 	ft_lstclear(&env->objects, free);
 	mlx_destroy_image(env->mlx, env->image.image);
 	mlx_destroy_window(env->mlx, env->mlx_window);
+	free(env->mlx);
 }
